1adfe9342346fdda9c6403aeef6afac35cab1eb8
misbashaikh2004 <misba6602@gmail.com>
2025-07-15 23:37:20 +0530

    First commit

---
+++ 3a.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter a no");
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+        printf("the no is even");
+    }
+    else{
+        printf("the no is odd");
+    }
+}

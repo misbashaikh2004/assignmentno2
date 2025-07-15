0827ecbd1fb58b70d1ed840d9b8f3189e155af2f
misbashaikh2004 <misba6602@gmail.com>
2025-07-15 23:13:59 +0530

    First commit

---
+++ 1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int  a;
+    printf("Enter a no");
+    scanf("%d",&a);
+    if(a>0)
+    printf("Number is positive\n");
+else
+printf("Number is not positive\n");
+    return 0;
+}
